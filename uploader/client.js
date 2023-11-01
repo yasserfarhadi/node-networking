e3a821c0700136fc6fac1d8bcbf24791a53d2694
yasser farhadi <yasser.farhadee@gmail.com>
2023-11-01 18:36:11 +0330

    showing upload process to the client(progress bar and percentage)

--- uploader/client.js
+++ uploader/client.js
@@ -2,23 +2,57 @@ const net = require('node:net');
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
+const clearLine = async (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = async (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
 const socket = net.createConnection(
   {
     host: '::1',
     port: 8080,
   },
+
   async () => {
     const filePath = process.argv[2];
     const fileName = path.basename(filePath);
     const fileHandle = await fs.open(filePath, 'r');
     const fileStream = fileHandle.createReadStream();
+    const fileSize = (await fileHandle.stat()).size;
+
+    let uploadedPercentage = 0;
+    let bytesUploded = 0;
 
     socket.write(`filename-${fileName}`);
 
-    fileStream.on('data', (data) => {
+    fileStream.on('data', async (data) => {
       const shouldWrite = socket.write(data);
       if (!shouldWrite) {
         fileStream.pause();
+      }
+      bytesUploded += data.length;
+      let newPercentage = Math.floor((bytesUploded / fileSize) * 100);
+      if (newPercentage % 5 === 0 && uploadedPercentage !== newPercentage) {
+        uploadedPercentage = newPercentage;
+        const progress =
+          '[--------------------]'
+            .substring(0, newPercentage / 4)
+            .replace(/-/g, '=') +
+          '[--------------------]'.substring(newPercentage / 4);
+        await moveCursor(0, -1);
+        await clearLine(0);
+        console.log(`Uploading ${fileName} ${progress} ${uploadedPercentage}%`);
       }
     });
 
